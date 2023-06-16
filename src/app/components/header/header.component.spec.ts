8898408a742b38ff56393d94fceb08f591e56ed0
kashif <kashifshah5898@gmail.com>
2023-06-16 09:51:05 +0500

    Test cases for header component

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the HeaderComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set currentRoute when the router navigates', () => {
+    router.navigate(['']).then(() => {
+      expect(component.currentRoute).toBe('/');
+    });
   });
 });
